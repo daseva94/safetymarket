51d850734b6bc89e31eecfc6c18afe7c18abe956
maria isabel roque muños <roque.estudiojuridico@gmail.com>
2021-10-23 17:25:29 -0500

    sprint 4

---
+++ safetymarkett/src/App.js
@@ -0,0 +1,13 @@
+import Login from "./Login";
+
+function App(props) {
+  return (
+    <div>
+      <Login />
+    </div>
+  );
+}
+
+export default App;
+
+<input type="submit" value="ingresar con Google" className="button_google"/>   
